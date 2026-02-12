169977c0ba0826ad14b0ce656f7652be65a540b2
Soharab Ali <sohrabalimail9@gmail.com>
2026-02-12 13:06:32 +0530

    add custom theme changer

--- ThemeChange/Theme.jsx
+++ ThemeChange/Theme.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 function Theme() {
 
     const [color,setColor]= useState('#FFFFFF')
+
+
     const changeColor=()=>{
         if(color==='#3E3E42')
         setColor('#FFFFFF')
